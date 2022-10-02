33d2e908639bd34d16e5fffa2066ff494068fc72
Harsh Mandaliya <32090768+hashmand@users.noreply.github.com>
2022-10-02 18:24:08 +0530

    JavaScript concepts
    JavaScript concepts

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.write(
+	'<script type="text/javascript" src="js/jquery.js"></script>' +		
+	'<script type="text/javascript" src="js/jquery.sticky.js"></script>' + 
+	'<script type="text/javascript" src="js/jquery-imagefill-min.js"></script>' +	
+	'<script type="text/javascript" src="js/bootstrap.bundle.js"></script>' +
+	'<script type="text/javascript" src="js/jquery-ui.min.js"></script>' +	
+	'<script type="text/javascript" src="js/external.js"></script>' + 
+	'<script type="text/javascript" src="js/bootstrap.js"></script>' +
+	'<script type="text/javascript" src="js/owl.carousel.min.js"></script>' +
+	'<script type="text/javascript" src="js/owl.carousel2.thumbs.min.js"></script>' +
+	'<script type="text/javascript" src="js/dropkick.js"></script>' +
+	'<script type="text/javascript" src="js/fastclick.min.js"></script>' +
+	'<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>' +	
+	'<script type="text/javascript" src="js/swiper.min.js"></script>' + 
+	'<script type="text/javascript" src="js/nouislider.js"></script>' +
+	'<script type="text/javascript" src="js/general.js"></script>' 
+	);
+
+
+	// '<script type="text/javascript" src="js/select2.min.js"></script>' +
+	// '<script type="text/javascript" src="js/scroll.js"></script>' +
+	// '<script type="text/javascript" src="js/bootstrap.min.js"></script>' 
+	// '<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/js/bootstrap-dialog.min.js"></script>' 
+	// '<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>' 
+	// '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>' 
+	// '<script type="text/javascript" src="js/dropkick.min.js"></script>' +
+	// '<script type="text/javascript" src="js/bootstrap.min.js"></script>' 
+	// '<script type="text/javascript" src="js/modernizr.js"></script>' +
+	// '<script type="text/javascript" src="js/respond.min.js"></script>' + 
+	// '<script type="text/javascript" src="js/swiper.jquery.min.js"></script>' 	
+	// '<script type="text/javascript" src="js/jquery.waypoints.min.js"></script>' +	
+	// '<script type="text/javascript" src="js/wNumb.js"></script>' +
+	// '<script type="text/javascript" src="js/nouislider.min.js"></script>' 
+	// '<script type="text/javascript" src="js/nouislider.min.js"></script>' 
+	// '<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>'  
+	// '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>' 
+	// '<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>'
